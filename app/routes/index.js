5a1d88bc3f60fe1505c4bfe8ae215a7dd1db2434
Elan Baharie and Max Reichman <elan+max@>
2017-05-31 09:58:19 -0700

    add update functionality and new review model

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
   actions: {
     saveRental3(params) {
-     var newRental = this.store.createRecord('rental', params);
-     newRental.save();
-     this.transitionTo('index');
-  },
-    destroyRental(rental) {
-      rental.destroyRecord();
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+    saveReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      newReview.save();
       this.transitionTo('index');
     }
   }
